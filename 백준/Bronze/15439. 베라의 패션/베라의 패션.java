b306466df52385c4830e9645b11f73712454c707
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2024-07-08 20:50:34 +0900

    [Bronze IV] Title: 베라의 패션, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        System.out.println(N*(N-1));
+    }
+}
