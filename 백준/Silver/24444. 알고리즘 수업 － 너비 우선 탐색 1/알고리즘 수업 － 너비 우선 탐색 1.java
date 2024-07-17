d201f3cce6963681fefb7b066f31f9d8483ecc90
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2024-07-17 23:23:00 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1840 ms, Memory: 107880 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static LinkedList<Integer>[] graph;
+    static boolean[] isVisit;
+    static Queue<Integer> queue;
+    static int[] sequence;
+    static int count = 1;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer setting = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(setting.nextToken());
+        int edge = Integer.parseInt(setting.nextToken());
+        int start = Integer.parseInt(setting.nextToken());
+
+        //초기화
+        isVisit = new boolean[n + 1];
+        graph = new LinkedList[n + 1];
+        sequence = new int[n + 1];
+        queue = new LinkedList<>();
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new LinkedList<>();
+            sequence[i] = 0;
+        }
+
+        //입력하기
+        for (int i = 0; i < edge; i++) {
+            StringTokenizer edgeInput = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(edgeInput.nextToken());
+            int b = Integer.parseInt(edgeInput.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 0; i < graph.length; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        bfs(start);
+
+        for (int i = 1; i < sequence.length; i++) {
+            System.out.println(sequence[i]);
+        }
+    }
+
+    static void bfs(int vertex) {
+        if (isVisit[vertex]) return;
+        isVisit[vertex] = true;
+        queue.add(vertex);
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            sequence[current] = count++;
+            for (int next : graph[current]) {
+                if (!isVisit[next]) {
+                    isVisit[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
