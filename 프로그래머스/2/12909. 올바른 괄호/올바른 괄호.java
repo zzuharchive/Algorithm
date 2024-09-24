36de58f55eff39d1b8026f454a5e7fafa7852783
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2024-09-24 17:07:47 +0900

    [level 2] Title: 올바른 괄호, Time: 16.12 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,9 +1,28 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        Stack<Character> parentheses = new Stack<>();
+        
+        // 여는 괄호는 stack에 push하고, 닫는 괄호는 pop을 한다.
+        for(int i=0; i<s.length();i++){
+            char here = s.charAt(i);
+            if(here == '('){
+                parentheses.push(here);
+            }else{
+                try{
+                    parentheses.pop();
+                }catch(EmptyStackException e){
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        
+        // 괄호가 모두 닫히지 않은 경우
+        if(parentheses.size()>0) answer = false;
 
         return answer;
     }
