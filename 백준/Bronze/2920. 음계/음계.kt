7b572dc583209e7fe1c6d459a53803cfd3f06438
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2025-01-16 07:06:49 +0900

    [Bronze II] Title: 음계, Time: 76 ms, Memory: 12324 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    var scale = readln()
+    var ascendingScale = "1 2 3 4 5 6 7 8"
+    var descendingScale = "8 7 6 5 4 3 2 1"
+
+    if(scale == ascendingScale) print("ascending")
+    else if(scale == descendingScale) print("descending")
+    else print("mixed")
+}
