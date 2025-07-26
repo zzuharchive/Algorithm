9030e5767689bb4b788582c3a16100d3023f39ce
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2025-07-26 23:22:10 +0900

    [level 1] Title: 신규 아이디 추천, Time: 12.44 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,30 +1,20 @@
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
-        String lowercase = new_id.toLowerCase();
-        String temp = "";
-        for(int i=0; i<lowercase.length(); i++){
-            if((lowercase.charAt(i) >= 'a' && lowercase.charAt(i)<='z')
-               || lowercase.charAt(i) >= '0' && lowercase.charAt(i) <= '9'
-               || lowercase.charAt(i) == '-' || lowercase.charAt(i) == '_'
-               || lowercase.charAt(i) == '.'){
-                temp += lowercase.charAt(i);
-            }
-        }
-        temp = temp.replaceAll("\\.{2,}", ".");
+        String answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9-_.]", "")
+                        .replaceAll("\\.+", ".");
         
-        if(temp.length() > 0 && temp.charAt(0)=='.') temp = temp.substring(1, temp.length());
-        if(temp.length() > 0&&temp.charAt(temp.length()-1)=='.') temp = temp.substring(0, temp.length()-1);
-        if(temp.length() == 0) temp = "a";
-        if(temp.length() >= 16) temp = temp.substring(0, 15);
-        if(temp.charAt(temp.length()-1)=='.') temp = temp.substring(0, temp.length()-1);
-         System.out.println(temp.length());
-        if(temp.length() <= 2) {
-            while(temp.length() < 3){
-               
-                temp += temp.charAt(temp.length()-1);
-            }
+        if(answer.startsWith(".")) answer = answer.substring(1);
+        if(answer.endsWith(".")) answer = answer.substring(0, answer.length()-1);
+        if(answer.isEmpty()) answer = "a";
+        if(answer.length() >= 16){
+            answer = answer.substring(0, 15);
+            if(answer.endsWith(".")) answer = answer.substring(0, answer.length()-1);
+        } 
+        
+        while(answer.length() < 3){
+            answer += answer.charAt(answer.length()-1);
         }
-        return temp;
+        return answer;
     }
 }
