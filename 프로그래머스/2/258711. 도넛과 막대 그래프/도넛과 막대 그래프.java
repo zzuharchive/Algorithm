267f716122ebcb8e1233cab0f1d2f30246433eda
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2026-06-17 23:16:02 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 20.39 ms, Memory: 189 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,29 +1,38 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[][] edges) {
-        int[] answer = new int[4];
-        int edgesNum = 0;
         
-        for(int i=0; i<edges.length; i++){
-            for(int j=0; j<edges[i].length; j++){
-                edgesNum = Math.max(edgesNum, edges[i][j]);
+        int n = 0;
+        for(int[] e : edges){
+            n = Math.max(n, e[0]);
+            n = Math.max(n, e[1]);
+        }
+        
+        int[] in = new int[n + 1];
+        int[] out = new int[n + 1];
+        
+        for(int[] e : edges){
+            out[e[0]]++;
+            in[e[1]]++;
+        }
+        
+        int[] answer = new int[4];
+      
+        for(int i=1; i<=n; i++){
+          
+            if(in[i] == 0 && out[i] >= 2){
+                answer[0] = i;
+            }else{
+                if(in[i] >= 1 && out[i] == 0){ // 막대
+                answer[2]++;
+                }
+                if(out[i] == 2){ // 8자
+                    answer[3]++;
+                }   
             }
         }
         
-        int[] in = new int[edgesNum + 1];
-        int[] out = new int[edgesNum + 1];
-        
-        for(int[] edge : edges){
-            out[edge[0]]++;
-            in[edge[1]]++;
-        }
-        
-        for(int i = 1; i<= edgesNum ; i++){
-            if(in[i] == 0 && out[i] >= 2) answer[0] = i;
-            else if(in[i] >= 1 && out[i] == 0) answer[2]++;
-            else if(in[i] >= 2 && out[i] >= 2) answer[3]++;
-        }
-        
-        answer[1] = out[answer[0]] - answer[2] - answer[3];
+        answer[1] = out[answer[0]] - (answer[2] + answer[3]); // 도넛
         
         return answer;
     }
