1439b6fbfd65e899e5a58796c510df1e00d5e4ae
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2026-06-18 22:39:41 +0900

    [level 3] Title: 네트워크, Time: 0.61 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,35 @@
+import java.util.*;
 class Solution {
     static boolean[] visited;
-  
     public int solution(int n, int[][] computers) {
-        
         int answer = 0;
-        visited = new boolean[n];
+        List<Integer>[] list = new ArrayList[n];
+        for(int i=0; i<n; i++){
+            list[i] = new ArrayList<>();
+            for(int j=0; j<n; j++){
+                if(computers[i][j]==1) list[i].add(j);
+            }
+        }
 
-        for(int i=0; i<visited.length;i++){
-            if(!visited[i]) {
-                dfs(i, computers);
-                answer++;
+        visited = new boolean[n];
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+                
+        for(int i=0; i<n; i++){
+            if(visited[i]) continue;
+            q.add(i);
+            visited[i] = true;
+            answer++;
+            
+            while(!q.isEmpty()){
+                int cur = q.poll();
+                for(int node : list[cur]){
+                    if(!visited[node]) {
+                        q.add(node);
+                        visited[node] = true;
+                    }
+                }
             }
         }
         return answer;
-    }
-    
-    static void dfs(int n, int[][] computers){
-       visited[n] = true;
-        for(int i=0; i<computers.length; i++){
-            if(!visited[i] && computers[n][i] == 1){
-                dfs(i, computers);
-            }
-        }
     }
 }
