cfd7964aa6fd9f0f59202c14762e4d0c3b30d82b
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2026-06-19 19:01:47 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,35 +1,37 @@
 import java.util.*;
 class Solution {
-    static boolean[] visited;
+    static int[] unionArr;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        List<Integer>[] list = new ArrayList[n];
-        for(int i=0; i<n; i++){
-            list[i] = new ArrayList<>();
-            for(int j=0; j<n; j++){
-                if(computers[i][j]==1) list[i].add(j);
+        
+        unionArr = new int[n];
+        for(int i = 0; i<n; i++){
+            unionArr[i] = i;
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j=0; j < n; j++){
+                if(i == j || computers[i][j] == 0) continue;
+                union(i, j);
             }
         }
-
-        visited = new boolean[n];
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-                
+        Set<Integer> set = new HashSet<>();
         for(int i=0; i<n; i++){
-            if(visited[i]) continue;
-            q.add(i);
-            visited[i] = true;
-            answer++;
-            
-            while(!q.isEmpty()){
-                int cur = q.poll();
-                for(int node : list[cur]){
-                    if(!visited[node]) {
-                        q.add(node);
-                        visited[node] = true;
-                    }
-                }
-            }
+            set.add(find(i));
         }
-        return answer;
+        
+        return set.size();
+    }
+    public int find(int x){
+        if(unionArr[x] == x) return x;
+        return unionArr[x] = find(unionArr[x]);
+    }
+    
+    public void union(int a, int b){
+        int rootA = find(a);
+        int rootB = find(b);
+        
+        if(rootA!=rootB){
+            unionArr[rootB] = rootA;
+        }
     }
 }
