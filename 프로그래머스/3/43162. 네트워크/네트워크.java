e166a68fdd77ede743eeaae188e6d7f38b07b094
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2026-09-14 22:16:47 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,35 @@
 import java.util.*;
 class Solution {
-    static int[] unionArr;
+    int[] arr;
     public int solution(int n, int[][] computers) {
         
-        unionArr = new int[n];
-        for(int i = 0; i<n; i++){
-            unionArr[i] = i;
+        arr = new int[n];
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = i;
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j=0; j < n; j++){
-                if(i == j || computers[i][j] == 0) continue;
-                union(i, j);
+        for(int i=0; i < n; i++){
+            for(int j = 0; j<n; j++){
+                if(i == j) continue;
+                if(computers[i][j] == 1) union(i, j);
             }
         }
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<n; i++){
+        Set<Integer>set = new HashSet<>();
+        for(int i=0; i<arr.length; i++){
             set.add(find(i));
         }
-        
         return set.size();
     }
-    public int find(int x){
-        if(unionArr[x] == x) return x;
-        return unionArr[x] = find(unionArr[x]);
-    }
     
-    public void union(int a, int b){
+    void union(int a, int b){
         int rootA = find(a);
         int rootB = find(b);
         
-        if(rootA!=rootB){
-            unionArr[rootB] = rootA;
-        }
+        if(rootA != rootB) arr[rootB] = rootA;
+    }
+    
+    int find(int a){
+        if(arr[a] != a) return arr[a] = find(arr[a]);
+        return arr[a];
     }
 }
