2c0934df8abee1a4e7a73fd85d886332051733b9
zzuharchive__ <145355985+zzuharchive@users.noreply.github.com>
2026-09-25 18:51:23 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        
+        if(n == 1) return 1;
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = dp[1] + 1;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-2] + dp[i-1])%1234567;
+        }
+        
+        return dp[n];
+    }
+}
